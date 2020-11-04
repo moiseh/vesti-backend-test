9060b5bd0c0194c58a6f14aef8eb8c306e38374d
Moises <moiseh@gmail.com>
2020-11-04 18:56:15 -0300

    Created stocks table migration

---
+++ src/database/migrations/2020_11_04_213606_stocks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class StocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+
+            $table->string('color_name')->nullable();
+            $table->string('size_name')->nullable();
+            $table->integer('quantity')->default(0);
+            $table->integer('order')->default(1);
+            $table->timestamps();
+            $table->string('sku')->nullable();
+            $table->double('last_update')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
